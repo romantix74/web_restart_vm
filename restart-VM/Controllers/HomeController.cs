583cd8cbcab7661a8d4b7ecbea429d7afc7e98db
romantix74@ <romantix74@list.ru>
2017-10-10 12:03:00 +0300

    2017.10.10  import-module problem

--- restart-VM/Controllers/HomeController.cs
+++ restart-VM/Controllers/HomeController.cs
@@ -30,20 +30,47 @@ namespace restart_VM.Controllers
             {
                 int _vmnum = Int32.Parse(Request.Form["vmnum"]);
 
-                using (PowerShell PowerShellInstance = PowerShell.Create())
+                using (PowerShell ps = PowerShell.Create())
                 {
+                    ps.AddScript("Set-ExecutionPolicy -ExecutionPolicy Unrestricted");
+                    ps.Invoke();
+
+                    // Add command to laod modules so that the CS cmdlets will work
+                    //PowerShellInstance.AddScript(@"Import-Module VMware.VimAutomation.Core;");
+                    //PowerShellInstance.Invoke();
+                    ps.Commands.AddCommand("Import-Module").AddArgument(@"VMware.VimAutomation.Core");
+                    ps.Invoke();
+
                     // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
                     // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
-                    PowerShellInstance.AddScript(string.Format("connect-viserver 192.168.46.10;  " +
-                            "get-vm pc_{0} | Restart-VM -confirm:$false; ", _vmnum) );
+                    ps.AddScript(string.Format(
+                            "Connect-VIServer 192.168.46.10;  " +
+                            "Get-VM pc_{0} | Restart-VM -confirm:$false; ", _vmnum) );
 
                     // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
                     //PowerShellInstance.AddParameter("param1", "parameter 1 value!");
                     // invoke execution on the pipeline (collecting output)
 
-                    Collection<PSObject> PSOutput = PowerShellInstance.Invoke();
+                    Collection<PSObject> PSOutput = ps.Invoke();
 
+                    // message for diagnostics
+                    _message = "diag NULL";
+
+                    // check the other output streams (for example, the error stream)
+                    if (ps.Streams.Error.Count > 0)
+                    {
+                        // error records were written to the error stream.
+                        //_message = PowerShellInstance.Streams.Error.ToString();
+                        _message = "";
+                        foreach (var i in ps.Streams.Error)
+                        {
+                            //display each product to console by using Display method in Farm Shop class
+                            _message = _message + ' ' + i.ToString();
+                        }
+                    }
+                    
                     // loop through each output object item
+                    
                     foreach (PSObject outputItem in PSOutput)
                     {
                         // if null object was dumped to the pipeline during the script then a null
@@ -52,11 +79,15 @@ namespace restart_VM.Controllers
                         {
                             //TODO: do something with the output item 
                             // outputItem.BaseOBject
-                            _message = outputItem.ToString();
+                            // _message = outputItem.ToString();
+                            _message = "Перезагрузка ВМ pc-" + Request.Form["vmnum"] + " прошла успешно" + " diag " + outputItem.ToString();
+                        }
+                        else {
+                            _message = "Перезагрузка ВМ pc-" + Request.Form["vmnum"] + " прошла успешно";
                         }
                     }
                 }
-                _message = "Перезагрузка ВМ pc-" + Request.Form["vmnum"] + " прошла успешно";
+                
             }
             catch (FormatException e)
             {
